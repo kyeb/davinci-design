79bc3549878c3bd07587ccf41e535925ae08817c
Kye Burchard <kyeb@mit.edu>
2024-04-15 00:33:29 -0700

    Much better viewer

--- src/components/viewer.tsx
+++ src/components/viewer.tsx
@@ -3,13 +3,11 @@ import * as classes from "../styles/editor.module.scss";
 
 import { ErrorBoundary } from "./error_boundary";
 import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
-import { Canvas, useLoader, useFrame } from "@react-three/fiber";
-import { Mesh } from "three";
-import { extend } from "@react-three/fiber";
-import { OrbitControls, TransformControls } from "three-stdlib";
-extend({ OrbitControls, TransformControls });
+import { Canvas, useLoader, useThree } from "@react-three/fiber";
+import { Mesh, Box3, Vector3 } from "three";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 
-const { Suspense, useRef } = React;
+const { Suspense, useRef, useEffect } = React;
 
 const ErrorView = () => {
   return <p>An error ocurred while rendering your model 😔</p>;
@@ -18,17 +16,27 @@ const ErrorView = () => {
 const STLModel = ({ url }) => {
   const stl = useLoader(STLLoader, url);
   const meshRef = useRef<Mesh>();
-  // useFrame((state, delta) => {
-  //   if (meshRef.current.rotation) {
-  //     meshRef.current.rotation.y -= delta / 2;
-  //   }
-  // });
+  const { scene } = useThree();
+
+  // Compute the center of our mesh and move it to 0, 0, 0
+  useEffect(() => {
+    if (meshRef.current) {
+      const bbox = new Box3().setFromObject(meshRef.current);
+
+      const center = new Vector3();
+      bbox.getCenter(center);
+
+      meshRef.current.position.x -= center.x;
+      meshRef.current.position.y -= center.y;
+      meshRef.current.position.z -= center.z;
+    }
+  }, [scene, stl]); // Depend on scene and stl to ensure update when necessary
 
   return (
     <>
       <mesh ref={meshRef} scale={[0.01, 0.01, 0.01]}>
         <primitive object={stl} attach="geometry"></primitive>
-        <meshStandardMaterial color="orange" roughness={0.2} metalness={0.3} />
+        <meshStandardMaterial color="orange" roughness={0.2} metalness={0.5} />
       </mesh>
     </>
   );
@@ -40,8 +48,10 @@ const STLViewer = ({ url }) => {
       <Canvas className={classes.canvas}>
         <ambientLight />
         <directionalLight color="white" position={[0, 0, 5]} />
-        {/* @ts-ignore extends don't get TypeScript support by default */}
-        {/* <orbitControls /> */}
+        <directionalLight color="white" position={[0, 0, -5]} />
+        {/* <directionalLight color="white" position={[0, -5, -5]} /> */}
+        <PerspectiveCamera makeDefault position={[3, 2, 5]} />
+        <OrbitControls rotateSpeed={0.5} />
         <Suspense fallback={null}>
           <STLModel url={url} />
         </Suspense>
