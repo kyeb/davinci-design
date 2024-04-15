18e08e2f4f617617d7c8804adbd978f352c28098
Kye Burchard <kyeb@mit.edu>
2024-04-15 00:20:55 -0700

    some spacing fixes

---
+++ src/components/error_boundary.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+
+type Props = {
+  fallback: React.ReactElement;
+  children: React.ReactElement;
+};
+
+type State = {
+  hasError: boolean;
+};
+
+class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    // Not implemented; someday error report
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export { ErrorBoundary };
--- src/components/viewer.tsx
+++ src/components/viewer.tsx
@@ -1,17 +1,29 @@
 import * as React from "react";
-import { Suspense, useRef } from "react";
+import * as classes from "../styles/editor.module.scss";
 
+import { ErrorBoundary } from "./error_boundary";
 import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
 import { Canvas, useLoader, useFrame } from "@react-three/fiber";
+import { Mesh } from "three";
+import { extend } from "@react-three/fiber";
+import { OrbitControls, TransformControls } from "three-stdlib";
+extend({ OrbitControls, TransformControls });
 
-function STLModel({ url }) {
+const { Suspense, useRef } = React;
+
+const ErrorView = () => {
+  return <p>An error ocurred while rendering your model 😔</p>;
+};
+
+const STLModel = ({ url }) => {
   const stl = useLoader(STLLoader, url);
-  const meshRef = useRef();
-  useFrame((state, delta) => {
-    if (meshRef.current.rotation) {
-      meshRef.current.rotation.y += delta;
-    }
-  });
+  const meshRef = useRef<Mesh>();
+  // useFrame((state, delta) => {
+  //   if (meshRef.current.rotation) {
+  //     meshRef.current.rotation.y -= delta / 2;
+  //   }
+  // });
+
   return (
     <>
       <mesh ref={meshRef} scale={[0.01, 0.01, 0.01]}>
@@ -20,17 +32,21 @@ function STLModel({ url }) {
       </mesh>
     </>
   );
-}
+};
 
 const STLViewer = ({ url }) => {
   return (
-    <Canvas style={{ background: "lightblue" }}>
-      <ambientLight />
-      <directionalLight color="white" position={[0, 0, 5]} />
-      <Suspense fallback={null}>
-        <STLModel url={url} />
-      </Suspense>
-    </Canvas>
+    <ErrorBoundary fallback={<ErrorView />}>
+      <Canvas className={classes.canvas}>
+        <ambientLight />
+        <directionalLight color="white" position={[0, 0, 5]} />
+        {/* @ts-ignore extends don't get TypeScript support by default */}
+        {/* <orbitControls /> */}
+        <Suspense fallback={null}>
+          <STLModel url={url} />
+        </Suspense>
+      </Canvas>
+    </ErrorBoundary>
   );
 };
 
