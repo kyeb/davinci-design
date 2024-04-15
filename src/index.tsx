f522f665c08bca1fb8a440341d58da45908121c9
Kye Burchard <kyeb@mit.edu>
2024-04-14 18:36:00 -0700

    fix Link

--- src/index.tsx
+++ src/index.tsx
@@ -3,34 +3,37 @@ import { createRoot } from "react-dom/client";
 import { Root } from "./routes/root";
 import { Editor } from "./routes/editor";
 import { Page } from "./components/page";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 
 import "./styles/global.scss";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "/",
-        element: (
-          <Page title="Home">
-            <a href="/editor">editor</a>
-          </Page>
-        ),
-      },
-      {
-        path: "/editor",
-        element: (
-          <Page title="Davinci Editor">
-            <Editor />
-          </Page>
-        ),
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "/",
+          element: (
+            <Page title="Home">
+              <Link to="/editor">editor</Link>
+            </Page>
+          ),
+        },
+        {
+          path: "/editor",
+          element: (
+            <Page title="Davinci Editor">
+              <Editor />
+            </Page>
+          ),
+        },
+      ],
+    },
+  ],
+  { basename: process.env.NODE_ENV === "production" ? "/davinci-design" : "" }
+);
 
 const container = document.getElementById("app");
 createRoot(container).render(
