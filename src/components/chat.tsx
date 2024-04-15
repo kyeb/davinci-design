01feabe48d30b09d7fb3172e8e1d6e99d4c753a8
Kye Burchard <kyeb@mit.edu>
2024-04-14 17:56:47 -0700

    placeholder for chat

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,7 +1,26 @@
 import * as React from "react";
 
+import * as classes from "../styles/chat.scss";
+document.body.className = classes.body;
+
 const Chat = () => {
-  return <h2>Chat</h2>;
+  return (
+    <div className="container">
+      <h2>Chat</h2>
+      <div>
+        <p>imagine some chat logs here</p>
+        <p>...</p>
+        <p>blah blah blah</p>
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Ask Davinci something..."
+          className="chat-input"
+        />
+      </div>
+    </div>
+  );
 };
 
 export { Chat };
