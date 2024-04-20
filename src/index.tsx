ecbb58582e78e4ce68b586a568cca0429b265a13
Kye Burchard <kyeb@mit.edu>
2024-04-20 15:57:42 -0700

    remove github pages deployment stuff

--- src/index.tsx
+++ src/index.tsx
@@ -8,40 +8,30 @@ import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 
 import "./styles/global.scss";
 
-// In prod, this site is currently deployed to a subpath on GitHub Pages
-// (https://kyeb.github.io/davinci-design/), which requires a base path to work
-// (otherwise the router gets *real* confused).
-
-// @ts-ignore Parcel makes the NODE_ENV variable available
-const basename = process.env.NODE_ENV === "production" ? "/davinci-design" : "";
-
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Root />,
-      children: [
-        {
-          path: "/",
-          element: (
-            <Page title="Home">
-              <Homepage />
-            </Page>
-          ),
-        },
-        {
-          path: "/editor",
-          element: (
-            <Page title="Davinci Editor">
-              <Editor />
-            </Page>
-          ),
-        },
-      ],
-    },
-  ],
-  { basename }
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <Page title="Home">
+            <Homepage />
+          </Page>
+        ),
+      },
+      {
+        path: "/editor",
+        element: (
+          <Page title="Davinci Editor">
+            <Editor />
+          </Page>
+        ),
+      },
+    ],
+  },
+]);
 
 const container = document.getElementById("app");
 createRoot(container).render(
