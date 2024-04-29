19b80bd081c6e503b6f7badb32af30385102072d
Kye Burchard <kyeb@mit.edu>
2024-04-28 17:03:48 -0700

    warning on setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This script is very incomplete, but an attempt was made. If someone else sets
+# this project, please fill this in.
+
+# Written for macOS
+
 brew install nodenv caddy
 
 cd frontend
